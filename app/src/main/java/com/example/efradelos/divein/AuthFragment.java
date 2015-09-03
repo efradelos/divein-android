f5b5f77f5dce66218580269453f402e030c04bde
Elias Fradelos <efradelos@urbanbound.com>
2015-09-03 10:39:05 -0500

    Moved auth to fragment

--- app/src/main/java/com/example/efradelos/divein/AuthFragment.java
+++ app/src/main/java/com/example/efradelos/divein/AuthFragment.java
@@ -1,41 +1,138 @@
 package com.example.efradelos.divein;
 
-import android.app.Activity;
-import android.net.Uri;
+import android.content.IntentSender;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.util.Log;
 
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.plus.Plus;
 
 public class AuthFragment extends Fragment
-        implements Firebase.AuthResultHandler, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-{
-    /* Is there a ConnectionResult resolution in progress? */
-    private boolean mIsResolving = false;
+    implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, Firebase.AuthResultHandler  {
+
+    private final String LOG_TAG = "EFX";
+    private static final int RC_GOOGLE_LOGIN = 1;
 
     /* Should we automatically resolve ConnectionResults when possible? */
     private boolean mShouldResolve = false;
+    private GoogleApiClient mGoogleApiClient;
+    private SignInListener mSignInListener;
 
-    private static final int RC_GOOGLE_LOGIN = 1;
+
+    public void setLoginListener(SignInListener listener) {
+        mSignInListener = listener;
+    }
+
+    public void loginInWithPassword(String username, String password) {
+        Constants.FIREBASE_REF.authWithPassword(username, password, this);
+    }
+
+    public void loginInWithGoogle() {
+        mShouldResolve = true;
+        mGoogleApiClient.connect();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(Plus.API)
                 .addScope(new Scope(Scopes.PLUS_LOGIN))
                 .build();
+        Log.d(LOG_TAG, "Created!");
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        new GetGoogleAuthTokenTask().execute();
+        mShouldResolve = false;
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // Could not connect to Google Play Services.  The user needs to select an account,
+        // grant permissions or resolve an error in order to sign in. Refer to the javadoc for
+        // ConnectionResult to see possible error codes.
+        Log.d(LOG_TAG, "onConnectionFailed:" + connectionResult);
+        if(mSignInListener != null) mSignInListener.signInFailed(true);
+
+        if (mShouldResolve) {
+            if (connectionResult.hasResolution()) {
+                try {
+                    connectionResult.startResolutionForResult(getActivity(), RC_GOOGLE_LOGIN);
+                } catch (IntentSender.SendIntentException e) {
+                    Log.e(LOG_TAG, "Could not resolve ConnectionResult.", e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onAuthenticated(AuthData authData) {
+        if(mSignInListener != null) mSignInListener.signInSucceeded();
+    }
+
+    @Override
+    public void onAuthenticationError(FirebaseError firebaseError) {
+        if(mSignInListener != null) mSignInListener.signInFailed(false);
+    }
+
+
+    private class GetGoogleAuthTokenTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String accountName = Plus.AccountApi.getAccountName(mGoogleApiClient);
+            String scopes = String.format("oauth2:%s", Scopes.PLUS_LOGIN);
+
+            try {
+                return GoogleAuthUtil.getToken(getActivity(), accountName, scopes);
+            } catch (Exception e) {
+                Log.e(LOG_TAG, "Error retrieving google auth token.", e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                Constants.FIREBASE_REF.authWithOAuthToken("google", result, AuthFragment.this);
+            }
+        }
+
+    }
+
+    public interface SignInListener {
+        void signInSucceeded();
+        void signInFailed(boolean autoTry);
     }
 }
--- app/src/main/java/com/example/efradelos/divein/LoginActivity.java
+++ app/src/main/java/com/example/efradelos/divein/LoginActivity.java
@@ -1,182 +1,91 @@
 package com.example.efradelos.divein;
 
+import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.content.IntentSender;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.google.android.gms.auth.GoogleAuthUtil;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.Scope;
-import com.google.android.gms.plus.Plus;
 
-public class LoginActivity extends AppCompatActivity
-        implements Firebase.AuthResultHandler, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class LoginActivity extends AppCompatActivity implements AuthFragment.SignInListener {
 
-    private static String TAG = "EFX";
-
-    /* Is there a ConnectionResult resolution in progress? */
-    private boolean mIsResolving = false;
-
-    /* Should we automatically resolve ConnectionResults when possible? */
-    private boolean mShouldResolve = false;
-
+    private final String LOG_TAG = "EFX";
+    private final String AUTH_FRAGMENT = "AUTH_FRAGMENT";
     private static final int RC_GOOGLE_LOGIN = 1;
 
     private EditText mUsername;
     private EditText mPassword;
     private ProgressBar mLoadingSpinner;
-    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Firebase.setAndroidContext(this);
-        Button signIn = (Button)findViewById(R.id.sign_in_button);
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(Plus.API)
-                .addScope(new Scope(Scopes.PLUS_LOGIN))
-                .build();
+        Button signIn = (Button) findViewById(R.id.sign_in_button);
 
-        mLoadingSpinner= (ProgressBar)findViewById(R.id.loading_spinner);
+        mLoadingSpinner = (ProgressBar) findViewById(R.id.loading_spinner);
 
-        mUsername = (EditText)findViewById(R.id.username_text);
-        mPassword = (EditText)findViewById(R.id.password_text);
+        mUsername = (EditText) findViewById(R.id.username_text);
+        mPassword = (EditText) findViewById(R.id.password_text);
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mLoadingSpinner.setVisibility(View.VISIBLE);
                 String username = mUsername.getText().toString();
                 String password = mPassword.getText().toString();
-                Constants.FIREBASE_REF.authWithPassword(username, password, (Firebase.AuthResultHandler) v.getContext());
+                AuthFragment fragment = getAuthFragment();
+                fragment.loginInWithPassword(username, password);
             }
         });
 
-        SignInButton google = (SignInButton)findViewById(R.id.google_sign_in_button);
+        SignInButton google = (SignInButton) findViewById(R.id.google_sign_in_button);
         google.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mLoadingSpinner.setVisibility(View.VISIBLE);
-                mShouldResolve = true;
-                mGoogleApiClient.connect();
+                AuthFragment fragment = getAuthFragment();
+                fragment.loginInWithGoogle();
             }
         });
-    }
+
+        AuthFragment fragment = new AuthFragment();
+        fragment.setLoginListener(this);
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.add(fragment, AUTH_FRAGMENT).commit();
+   }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mGoogleApiClient.disconnect();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Log.d(TAG, "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
 
         if (requestCode == RC_GOOGLE_LOGIN) {
-            // If the error resolution was not successful we should not resolve further.
-            if (resultCode != RESULT_OK) {
-                mShouldResolve = false;
-            }
-
-            mIsResolving = false;
-            mGoogleApiClient.connect();
+            mLoadingSpinner.setVisibility(View.VISIBLE);
+            AuthFragment fragment = getAuthFragment();
+            fragment.loginInWithGoogle();
         }
     }
 
     @Override
-    public void onAuthenticated(AuthData authData) {
+    public void signInSucceeded() {
         mLoadingSpinner.setVisibility(View.GONE);
         startActivity(new Intent(this, MainActivity.class));
     }
 
     @Override
-    public void onAuthenticationError(FirebaseError firebaseError) {
+    public void signInFailed(boolean autoTry) {
         mLoadingSpinner.setVisibility(View.GONE);
-        Log.e(TAG, firebaseError.toString());
-        Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_LONG).show();
+        if (!autoTry) Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_LONG).show();
+
     }
 
-    @Override
-    public void onConnectionSuspended(int i) { }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        new GetIdTokenTask().execute();
-        Log.d(TAG, "onConnected:" + bundle);
-        mShouldResolve = false;
-    }
-
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        // Could not connect to Google Play Services.  The user needs to select an account,
-        // grant permissions or resolve an error in order to sign in. Refer to the javadoc for
-        // ConnectionResult to see possible error codes.
-        Log.d(TAG, "onConnectionFailed:" + connectionResult);
-
-        mLoadingSpinner.setVisibility(View.GONE);
-        if (!mIsResolving && mShouldResolve) {
-            if (connectionResult.hasResolution()) {
-                try {
-                    mLoadingSpinner.setVisibility(View.VISIBLE);
-                    connectionResult.startResolutionForResult(this, RC_GOOGLE_LOGIN);
-                    mIsResolving = true;
-                } catch (IntentSender.SendIntentException e) {
-                    Log.e(TAG, "Could not resolve ConnectionResult.", e);
-                    mIsResolving = false;
-                    mGoogleApiClient.connect();
-                }
-            }
-        }
-    }
-
-    private class GetIdTokenTask extends AsyncTask<Void, Void, String> {
-
-        @Override
-        protected String doInBackground(Void... params) {
-            String accountName = Plus.AccountApi.getAccountName(mGoogleApiClient);
-            String scopes = String.format("oauth2:%s", Scopes.PLUS_LOGIN);
-
-            try {
-                Log.i(TAG, "Retrieving google auth token.");
-                return GoogleAuthUtil.getToken(LoginActivity.this, accountName, scopes);
-            } catch (Exception e) {
-                Log.e(TAG, "Error retrieving google auth token.", e);
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            if (result != null) {
-                Log.i(TAG, "Retrieved google auth token: " + result);
-                Constants.FIREBASE_REF.authWithOAuthToken("google", result, LoginActivity.this);
-            }
-        }
-
+    private AuthFragment getAuthFragment() {
+        return (AuthFragment)getFragmentManager().findFragmentByTag(AUTH_FRAGMENT);
     }
 }
